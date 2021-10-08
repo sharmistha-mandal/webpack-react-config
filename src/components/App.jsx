142cee67a3cedbf9683a55f19a9df1c5eab46776
sharmistha.mandal <sharmistha.mandal@LSUK-0220>
2021-10-08 16:41:48 +0100

    add image support to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
+import logo from "../images/bag-logo.svg";
 
 const App = () => {
   return (
     <>
-      <section className="hello"></section>
+      <section className="hero"></section>
       <main>
         <section>
           <h1>hello React</h1>
         </section>
+        <img src={logo} alt="Buy a gift Logo" width="250" />
         <Recipes />
       </main>
     </>
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[hash][ext][query]",
   },
 
   devtool: "source-map",
@@ -32,9 +33,27 @@ module.exports = {
         },
       },
       {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset",
+        /* it will generate inline base64 image if it is less than 8kb. you can extend this limit (3o byte) by using following code 
+        parser:{
+          dataUrlCondition:{
+            maxSize:30 * 1024,
+          }
+        }
+        
+        */
+
+        /* asset/resource will put all of your images inside images folder regardless of size */
+        /* asset/inline will generate the baseurl64 format */
+      },
+      {
         test: /\.(s[ac]|c)ss$/,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" },
+          },
           "css-loader",
           "postcss-loader",
           "sass-loader",
